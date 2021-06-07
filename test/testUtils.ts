a4e8e1b9b58fb00408efb7f29797b4829fcb01ab
Rory Powis <rpowis@users.noreply.github.com>
2021-06-07 15:48:27 +0200

    Fix lint-staged commands

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -14,7 +14,7 @@ const Providers = ({ children }) => {
   // )
 };
 
-const customRender = (ui: React.ReactElement, options = {}) =>
+const customRender = (ui: React.ReactElement, options = {}): void =>
   render(ui, { wrapper: Providers, ...options });
 
 // re-export everything
