f04f68eddaeb7bac856106eec0e44fc908166746
Chris Knight <chris@concretehermit.com>
2021-06-10 17:25:07 +0100

    add classes missing from button to allow it to stretch and remain centered

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const buttonClasses = [
-  "inline-flex items-center",
+  "inline-flex items-center justify-center",
   "border rounded",
   "text-center leading-snug",
   "focus:outline-none focus:ring-2 focus:ring-offset-2",
@@ -10,14 +10,14 @@ const buttonClasses = [
 ].join(" ");
 
 const STYLES = {
-  PRIMARY: `${buttonClasses} text-white bg-green-600`,
+  PRIMARY: `${buttonClasses} text-white bg-green-600 border-green-600 hover:bg-green-700 focus:ring-green-500`,
   SECONDARY: `${buttonClasses}`,
 };
 
 const SIZES = {
-  SMALL: ``,
+  SMALL: `py-1 px-4`,
   MEDIUM: `py-2 px-4`,
-  LARGE: ``,
+  LARGE: `py-3 px-8`,
 };
 
 const Button = (props) => {
