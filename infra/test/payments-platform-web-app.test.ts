622c38e6dca1c05b0028f3dc2d2d8d9403cbf972
Rory Powis <rpowis@users.noreply.github.com>
2021-06-14 17:16:46 +0200

    Fix typo in test file name

--- infra/test/payments-platform-web-app.test.ts
+++ infra/test/payments-platform-web-app.test.ts
@@ -8,18 +8,13 @@ import * as PaymentsPlatformWebApp from "../lib/payments-platform-web-app-stack"
 
 test("Empty Stack", () => {
   const app = new cdk.App();
+
   // WHEN
   const stack = new PaymentsPlatformWebApp.PaymentsPlatformWebAppStack(
     app,
     "MyTestStack"
   );
+
   // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT
-    )
-  );
+  expectCDK(stack).to(matchTemplate({ Resources: {} }, MatchStyle.EXACT));
 });
