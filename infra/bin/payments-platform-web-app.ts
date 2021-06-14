a442d0d58f41a6abb0e4676e217abbcad3050813
Tom VN <tom@tomvn.com>
2021-06-14 20:16:25 +0200

    wire up dns

--- infra/bin/payments-platform-web-app.ts
+++ infra/bin/payments-platform-web-app.ts
@@ -3,6 +3,7 @@ import "source-map-support/register";
 import * as path from "path";
 import * as cdk from "@aws-cdk/core";
 import { PaymentsPlatformWebAppStack } from "../lib/payments-platform-web-app-stack";
+import { PaymentsPlatformWebAppLinkStack } from "../lib/payments-platform-web-app-stack";
 import { Builder } from "@sls-next/lambda-at-edge";
 
 const nextConfigPath = path.resolve("..");
@@ -17,13 +18,21 @@ builder
   .build()
   .then(() => {
     const app = new cdk.App();
-    new PaymentsPlatformWebAppStack(app, "PaymentsPlatformWebAppStack", {
+    const cfStack = new PaymentsPlatformWebAppStack(app, "PaymentsPlatformWebAppStack", {
       env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: "us-east-1",
       },
       stackName: app.node.tryGetContext("stack_name"),
       description: app.node.tryGetContext("stack_description"),
+    });
+    new PaymentsPlatformWebAppLinkStack(app, 'PaymentsPlatformWebAppLinkStack', {
+      env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+      },
+      stackName: app.node.tryGetContext('stack_name'),
+      description: app.node.tryGetContext('stack_description'),
     });
   })
   .catch((e) => {
--- infra/lib/payments-platform-web-app-stack.ts
+++ infra/lib/payments-platform-web-app-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from "@aws-cdk/core";
 import { NextJSLambdaEdge } from "@sls-next/cdk-construct";
 import * as ssm from "@aws-cdk/aws-ssm";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as alias from "@aws-cdk/aws-route53-targets";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import * as certificatemanager from '@aws-cdk/aws-certificatemanager';
+import { SSMParameterReader } from './ssm-parameter-reader';
+import { RecordTarget } from "@aws-cdk/aws-route53";
 
 export class PaymentsPlatformWebAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -42,5 +47,43 @@ export class PaymentsPlatformWebAppStack extends cdk.Stack {
     new cdk.CfnOutput(this, "DomainName", {
       value: `https://${serverlessNext.distribution.domainName}`,
     });
+  }
+}
+
+export class PaymentsPlatformWebAppLinkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const branch = this.node.tryGetContext("branch");
+
+    const clearTreasuryCoUkDomainName = `${branch === "main" ? "" : branch + ".nonprod"}.cleartreasury.co.uk`;
+
+    const domainNameSsmReader = new SSMParameterReader(this, 'DomainNameSsmReader', {
+      parameterName: '/payments-platform-web-app/domainName',
+      region: 'us-east-1'
+    });
+    const domainName: string = domainNameSsmReader.getResponseField('Parameter.Value');
+
+    const distributionIdSsmReader = new SSMParameterReader(this, 'DistributionIdSsmReader', {
+      parameterName: '/payments-platform-web-app/domainName',
+      region: 'us-east-1'
+    });
+    const distributionId: string = distributionIdSsmReader.getResponseField('Parameter.Value');
+
+    const zone = route53.HostedZone.fromHostedZoneAttributes(this, "Zone", {
+      hostedZoneId: cdk.Fn.importValue(`${branch}:dns:ClearTreasuryCoUk:hostedZoneId`),
+      zoneName: cdk.Fn.importValue(`${branch}:dns:ClearTreasuryCoUk:zoneName`),
+    })
+
+    const cloudFront = cloudfront.Distribution.fromDistributionAttributes(this, "cloudFront", {
+      distributionId: distributionId,
+      domainName: domainName,
+    });
+
+    const dnsRecord = new route53.CnameRecord(this, "fxopsDns", {
+      zone: zone,
+      recordName: clearTreasuryCoUkDomainName,
+      domainName: domainName,
+    })
   }
 }
