47f8d4ef11682fa2e0b33f03b8927e46b60f1206
Rory Powis <rpowis@users.noreply.github.com>
2021-06-18 08:28:37 +0200

    Use new MFA user pool

--- configureAmplify.ts
+++ configureAmplify.ts
@@ -3,16 +3,16 @@ import Amplify from "aws-amplify";
 export const config = {
   Auth: {
     // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-    identityPoolId: "eu-west-2:35055f54-cc32-4f20-9c61-7df201ae0f58",
+    identityPoolId: "eu-west-2:63f56377-1b3d-4778-ab9c-4f6eb655b8f3",
 
     // REQUIRED - Amazon Cognito Region
     region: "eu-west-2",
 
     // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: "eu-west-2_QUpunH3eQ",
+    userPoolId: "eu-west-2_KJVpOC6Th",
 
     // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: "3628dkijg32ktv9qj9qp9gfkqp",
+    userPoolWebClientId: "57qp2vvtsidncdb0anpbpb8qqr",
 
     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
     mandatorySignIn: true,
