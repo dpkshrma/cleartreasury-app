408d87f5a8f3f9ac422ac76f289e431bc0b9f539
Emre Gozel <emre@gozel.org>
2021-07-05 14:41:20 +0300

    linter edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const designSystem = require("@clear-treasury/design-system/tailwind.config");
 
-const theme = require('./src/theme/theme');
+const theme = require("./src/theme/theme");
 
 module.exports = {
   ...designSystem,
