a01caedc2bf4e6e997b40ef2781eab49e3462a42
Rory Powis <rpowis@users.noreply.github.com>
2021-07-19 11:07:00 +0200

    Update appsync api key

--- configureAmplify.ts
+++ configureAmplify.ts
@@ -22,7 +22,7 @@ export const config = {
       "https://ojljqi4kdjb3ve2qk5ghmw3veu.appsync-api.eu-west-2.amazonaws.com/graphql",
     aws_appsync_region: "eu-west-2",
     aws_appsync_authenticationType: "API_KEY",
-    aws_appsync_apiKey: "da2-yidxxrxde5aq5krfse5b2oqspm",
+    aws_appsync_apiKey: "da2-glctq74nxbchbhmgore54nm7wu",
   },
 };
 
