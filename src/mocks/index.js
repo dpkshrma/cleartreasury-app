b6283fec911d793af40665f92a830b511359789b
Rory Powis <rpowis@users.noreply.github.com>
2021-07-20 14:52:47 +0200

    Bypass msw server-side warnings

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,6 +1,6 @@
 if (typeof window === "undefined") {
   const { server } = require("./server");
-  server.listen();
+  server.listen({ onUnhandledRequest: "bypass" });
 } else {
   const { worker } = require("./browser");
   worker.start({ onUnhandledRequest: "bypass" });
