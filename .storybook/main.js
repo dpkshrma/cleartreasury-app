ccd1ef26e7bd454ee0af6fbcef492ebf3fa18e14
Rory Powis <rpowis@users.noreply.github.com>
2021-07-21 16:02:49 +0200

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+};
