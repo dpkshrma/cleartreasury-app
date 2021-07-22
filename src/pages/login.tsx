3d325cad87f33bae5b0ec336c024567af669e8d6
Rory Powis <rpowis@users.noreply.github.com>
2021-07-22 14:25:04 +0200

    Simplify and tidy up login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,153 +4,171 @@ import Router from "next/router";
 import { Auth, withSSRContext } from "aws-amplify";
 import { Button, Input, Alert } from "@clear-treasury/design-system";
 import Page from "../components/page/Page";
-import { MailIcon, InformationCircleIcon } from "@heroicons/react/outline";
 
-const initialFormState = {
-  username: "",
-  password: "",
-  email: "",
-  authCode: "",
-  formType: "signIn",
-  alert: false,
-  alertMessage: "",
-  alertStatus: null,
-  alertIcon: MailIcon,
+type Error = {
+  message: string;
 };
+
+type Errors = {
+  alert?: Error;
+  email?: Error;
+  password?: Error;
+  newPassword?: Error;
+};
+
+interface FormState {
+  formType: "signIn" | "newPasswordRequired";
+  errors: Errors;
+}
 
 function Login(props) {
   const [user, setUser]: any = React.useState();
-  const [formState, setFormState] = React.useState(initialFormState);
-  const { formType } = formState;
+  const [loading, setLoading] = React.useState(false);
+  const [formState, setFormState] = React.useState<FormState>({
+    formType: "signIn",
+    errors: {},
+  });
+
   const userEmail = React.useRef<HTMLInputElement | null>(null);
   const userPassword = React.useRef<HTMLInputElement | null>(null);
   const userNewPassword = React.useRef<HTMLInputElement | null>(null);
-  const userAuthCode = React.useRef<HTMLInputElement | null>(null);
-  const passwordRegex =
-    /^(?=.{6,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/;
-  const [loading, setLoading] = React.useState(false);
 
-  function handleSubmit(e: any) {
-    e.preventDefault();
+  function handleSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+
+    const isValid = validateForm();
+    if (!isValid) return false;
+
     if (formState.formType == "signIn") {
       signIn();
-    } else if (formState.formType == "confirmSignIn") {
-      confirmSignIn();
     } else if (formState.formType == "newPasswordRequired") {
-      setFormState(() => ({ ...formState, formType: "confirmSignIn" }));
       newPasswordRequired();
     }
   }
 
-  async function signIn() {
+  function validateForm() {
+    const errors: Errors = {};
+
+    if (!userEmail?.current?.value) {
+      errors.email = { message: "You must enter an email" };
+    }
+
+    if (formState.formType === "signIn" && !userPassword?.current?.value) {
+      errors.password = {
+        message: "You must enter a password",
+      };
+    }
+
     if (
-      userEmail.current !== null &&
-      userPassword.current.value.match(passwordRegex)
+      formState.formType === "newPasswordRequired" &&
+      !userNewPassword?.current?.value
     ) {
-      if (formState.alertStatus) {
-        setFormState(() => ({
-          ...formState,
-          alert: false,
-        }));
-      }
-      setLoading(true);
-      await Auth.signIn(userEmail.current.value, userPassword.current.value)
-        .then((res: any) => {
-          if (res.challengeName == "NEW_PASSWORD_REQUIRED") {
-            setFormState(() => ({
-              ...formState,
-              formType: "newPasswordRequired",
-            }));
-            setUser(res);
-            setLoading(false);
-          } else {
-            setUser(res);
-            props.setContext(res);
-            Router.push("/authenticate");
-          }
-        })
-        .catch((err: any) => {
-          setFormState(() => ({
-            ...formState,
-            alert: true,
-            alertMessage: err.message,
-            alertStatus: Alert.Status.CRITICAL,
-          }));
-          setLoading(false);
-        });
+      errors.newPassword = { message: "You must enter a password" };
+    }
+
+    if (Object.keys(errors).length) {
+      setFormState({ ...formState, errors });
+      return false;
+    } else {
+      return true;
     }
   }
 
-  async function confirmSignIn() {
+  async function signIn() {
     setLoading(true);
-    const userData = await Auth.confirmSignIn(user, userAuthCode.current.value)
-      .then((res: any) => {
-        setLoading(false);
-        setUser(res);
-        return res;
-      })
-      .catch((err: any) => {
-        setFormState(() => ({
-          ...formState,
-          alert: true,
-          alertMessage: err.message,
-          alertStatus: Alert.Status.CRITICAL,
-        }));
-        setLoading(false);
-      });
 
-    if (userData) {
-      Router.push("/");
-    }
-  }
-
-  async function newPasswordRequired() {
-    if (userNewPassword.current.value.match(passwordRegex)) {
-      setLoading(true);
-      const userData = await Auth.completeNewPassword(
-        user,
-        userNewPassword.current.value,
-        { email: userEmail.current.value }
-      )
-        .then((res: any) => {
-          return res;
-        })
-        .catch(() => {
-          setLoading(false);
-        });
-
-      setUser(userData);
-      setFormState(() => ({ ...formState, formType: "confirmSignIn" }));
-    }
-  }
-
-  async function resendConfirmationCode() {
-    setLoading(true);
     try {
-      await Auth.resendSignUp(user.username).then(() => {
+      const authData = await Auth.signIn(
+        userEmail.current.value,
+        userPassword.current.value
+      );
+
+      if (authData.challengeName == "NEW_PASSWORD_REQUIRED") {
         setFormState(() => ({
           ...formState,
-          alert: true,
-          alertMessage: "A code has been sent to your phone number again",
-          alertIcon: InformationCircleIcon,
-          alertStatus: Alert.Status.PRIMARY,
+          formType: "newPasswordRequired",
         }));
+        setUser(authData);
         setLoading(false);
-      });
-    } catch (err) {
+      } else {
+        setUser(authData);
+        props.setContext(authData);
+        Router.push("/authenticate");
+      }
+    } catch (error) {
       setFormState(() => ({
         ...formState,
-        alert: true,
-        alertMessage: err.message,
-        alertStatus: Alert.Status.CRITICAL,
+        errors: { alert: { message: error.message } },
       }));
+    } finally {
       setLoading(false);
     }
   }
 
+  async function newPasswordRequired() {
+    setLoading(true);
+
+    try {
+      const userData = await Auth.completeNewPassword(
+        user,
+        userNewPassword.current.value,
+        { email: userEmail.current.value }
+      );
+
+      setUser(userData);
+    } catch (error) {
+      setFormState(() => ({
+        ...formState,
+        errors: { alert: { message: error.message } },
+      }));
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const SignInForm = () => (
+    <>
+      <Input
+        type="password"
+        name="password"
+        label="Password"
+        placeholder="Enter your password"
+        ref={userPassword}
+        errors={formState.errors}
+      />
+
+      <Link href="/reset-password">
+        <a className="text-green-700 text-sm mb-16 cursor-pointer">
+          Forgot your password?
+        </a>
+      </Link>
+
+      <Button size={Button.Size.LARGE} loading={loading}>
+        Sign in
+      </Button>
+    </>
+  );
+
+  const SetPasswordForm = () => (
+    <>
+      <Input
+        type="password"
+        name="newPassword"
+        label="Change your password"
+        placeholder="Enter your new password"
+        ref={userNewPassword}
+        errors={formState.errors}
+      />
+
+      <Button size={Button.Size.LARGE} loading={loading}>
+        Set new password
+      </Button>
+    </>
+  );
+
   return (
     <Page>
-      <div className="login-wrapper h-screen flex w-full bg-teal-700">
+      <div className="h-screen flex w-full bg-teal-700">
         <div className="max-w-md w-full m-auto p-0">
           <img
             className="h-12 w-full mb-8"
@@ -160,82 +178,35 @@ function Login(props) {
 
           <div className="p-6 space-y-6 bg-white rounded-md flex justify-center flex-col shadow-md">
             <h1 className="block w-full text-center text-gray-800 text-2xl">
-              Sign in to your account
+              {formState.formType === "newPasswordRequired"
+                ? "Set your password"
+                : "Sign in to your account"}
             </h1>
 
             <form
               onSubmit={handleSubmit}
               className="flex justify-center flex-col space-y-6"
             >
-              {formState.alert && (
+              {formState.errors.alert && (
                 <Alert
-                  icon={formState.alertIcon}
-                  status={formState.alertStatus}
-                  text={formState.alertMessage}
+                  text={formState.errors.alert.message}
+                  status={Alert.Status.CRITICAL}
                 />
               )}
 
-              {formType === "signIn" && (
-                <React.Fragment>
-                  <Input
-                    name="email"
-                    type="email"
-                    label="Email address"
-                    placeholder="Enter your email"
-                    ref={userEmail}
-                  />
-                  <Input
-                    name="password"
-                    type="password"
-                    label="Password"
-                    placeholder="Enter your password"
-                    ref={userPassword}
-                  />
-                  <Link href="/resettingpassword">
-                    <a className="text-gray-600 text-sm mb-16 cursor-pointer">
-                      Forgot your password?
-                    </a>
-                  </Link>
-                  <Button size={Button.Size.LARGE} loading={loading}>
-                    Sign in
-                  </Button>
-                </React.Fragment>
-              )}
+              <Input
+                type="email"
+                name="email"
+                label="Email address"
+                placeholder="Enter your email"
+                ref={userEmail}
+                disabled={formState.formType === "newPasswordRequired"}
+                errors={formState.errors}
+              />
 
-              {formType === "confirmSignIn" && (
-                <React.Fragment>
-                  <Input
-                    name="authCode"
-                    type="text"
-                    label="Enter verification code"
-                    placeholder="Enter your code"
-                    ref={userAuthCode}
-                  />
-                  <span
-                    onClick={resendConfirmationCode}
-                    className="cursor-pointer text-gray-600 text-sm mb-16"
-                  >
-                    Resend verification code
-                  </span>
-                  <Button size={Button.Size.LARGE} loading={loading}>
-                    Sign in
-                  </Button>
-                </React.Fragment>
-              )}
-
-              {formType === "newPasswordRequired" && (
-                <React.Fragment>
-                  <Input
-                    name="password"
-                    type="password"
-                    label="Change your password"
-                    placeholder="Enter your new password"
-                    ref={userNewPassword}
-                  />
-                  <Button size={Button.Size.LARGE} loading={loading}>
-                    Set new password
-                  </Button>
-                </React.Fragment>
+              {formState.formType === "signIn" && <SignInForm />}
+              {formState.formType === "newPasswordRequired" && (
+                <SetPasswordForm />
               )}
             </form>
           </div>
