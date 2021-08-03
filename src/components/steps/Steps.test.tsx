ed6e60ff85546ea331d57631298f612d61eb5c93
davidmaisuradze <dmaisuradze@s-pro.io>
2021-08-04 02:23:45 +0400

    added more tests for Steps component

--- src/components/steps/Steps.test.tsx
+++ src/components/steps/Steps.test.tsx
@@ -1,11 +1,47 @@
 import * as React from "react";
 import { render } from "../../../test/testUtils";
 import { StepsStory } from "./Steps.stories";
+import { act, cleanup, fireEvent, getByText } from "@testing-library/react";
 
 describe("Steps", () => {
-  it("matches snapshot for nested children", () => {
-    const { asFragment } = render(<StepsStory />);
+  let component: DocumentFragment;
+  let firstChild: HTMLCanvasElement;
 
-    expect(asFragment()).toMatchSnapshot();
+  beforeEach(() => {
+    const { asFragment } = render(<StepsStory />);
+    component = asFragment();
+    firstChild = component.firstChild as HTMLCanvasElement;
+  });
+  afterEach(cleanup);
+
+  it("matches snapshot for nested children", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  describe("matches snapshot fragment", () => {
+    it("for Amount text", () => {
+      const span = getByText(firstChild, "Amount");
+      expect(span).not.toBe(null);
+      expect(span).toMatchSnapshot();
+    });
+
+    it("for Step 1 form", () => {
+      const form = firstChild.querySelector("form");
+
+      expect(form).not.toBe(null);
+      expect(form).toMatchSnapshot();
+    });
+
+    it("for Step 2 form after Step 1 submit", async () => {
+      const button = getByText(firstChild, "Continue");
+      expect(button).not.toBe(null);
+
+      await act(async () => {
+        fireEvent.click(button);
+      });
+
+      const beneficiaryForm = firstChild.querySelector("form");
+      expect(beneficiaryForm).toMatchSnapshot();
+    });
   });
 });
