cfdcfb84e5fc5702dc5b05f74e2a16d27b067b4a
Rory Powis <rpowis@users.noreply.github.com>
2021-08-04 08:51:20 +0200

    Clean up Steps tests

--- src/components/steps/Steps.stories.tsx
+++ src/components/steps/Steps.stories.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Meta } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 import Steps from "./Steps";
-import Page from "../page/Page";
 import Step from "./Step";
 
 export default {
@@ -19,8 +18,8 @@ const Form: React.FC<{
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>{name}</p>
+    <form data-testid="form" onSubmit={handleSubmit}>
+      <h1>{name}</h1>
       <button>Continue</button>
     </form>
   );
@@ -31,17 +30,15 @@ const Form2A = (props) => <Form name="Form 2A" {...props} />;
 const Form2B = (props) => <Form name="Form 2B" {...props} />;
 const Form3 = (props) => <Form name="Form 3" {...props} />;
 
-export const StepsStory: React.VFC<Record<string, never>> = () => (
-  <Page>
-    <Steps nav={<Step />}>
-      <Steps.Step title="Amount" form={<Form1 />} />
-      <Steps.Step title="Beneficiary">
-        <Steps>
-          <Steps.Step form={<Form2A />} />
-          <Steps.Step form={<Form2B />} />
-        </Steps>
-      </Steps.Step>
-      <Steps.Step title="Confirm & Pay" form={<Form3 />} />
-    </Steps>
-  </Page>
+export const Default: Story<Record<string, unknown>> = () => (
+  <Steps nav={<Step />}>
+    <Steps.Step title="Amount" form={<Form1 />} />
+    <Steps.Step title="Beneficiary">
+      <Steps>
+        <Steps.Step form={<Form2A />} />
+        <Steps.Step form={<Form2B />} />
+      </Steps>
+    </Steps.Step>
+    <Steps.Step title="Confirm & Pay" form={<Form3 />} />
+  </Steps>
 );
--- src/components/steps/Steps.test.tsx
+++ src/components/steps/Steps.test.tsx
@@ -1,47 +1,19 @@
 import * as React from "react";
 import { render } from "../../../test/testUtils";
-import { StepsStory } from "./Steps.stories";
-import { act, cleanup, fireEvent, getByText } from "@testing-library/react";
+import { Default } from "./Steps.stories";
+import { fireEvent, screen } from "@testing-library/react";
 
 describe("Steps", () => {
-  let component: DocumentFragment;
-  let firstChild: HTMLCanvasElement;
-
-  beforeEach(() => {
-    const { asFragment } = render(<StepsStory />);
-    component = asFragment();
-    firstChild = component.firstChild as HTMLCanvasElement;
-  });
-  afterEach(cleanup);
-
   it("matches snapshot for nested children", () => {
-    expect(component).toMatchSnapshot();
+    const { asFragment } = render(<Default />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  describe("matches snapshot fragment", () => {
-    it("for Amount text", () => {
-      const span = getByText(firstChild, "Amount");
-      expect(span).not.toBe(null);
-      expect(span).toMatchSnapshot();
-    });
+  it("navigate to next step when submitting a form", async () => {
+    render(<Default />);
 
-    it("for Step 1 form", () => {
-      const form = firstChild.querySelector("form");
+    fireEvent.click(screen.getByText("Continue"));
 
-      expect(form).not.toBe(null);
-      expect(form).toMatchSnapshot();
-    });
-
-    it("for Step 2 form after Step 1 submit", async () => {
-      const button = getByText(firstChild, "Continue");
-      expect(button).not.toBe(null);
-
-      await act(async () => {
-        fireEvent.click(button);
-      });
-
-      const beneficiaryForm = firstChild.querySelector("form");
-      expect(beneficiaryForm).toMatchSnapshot();
-    });
+    expect(screen.getByRole("heading")).toHaveTextContent("Form 2A");
   });
 });
