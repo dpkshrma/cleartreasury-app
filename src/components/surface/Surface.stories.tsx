61bf21f42bf6f8d37d6c74ac36c0064bd6f0456a
Emre Gozel <emre@gozel.org>
2021-08-04 10:40:18 +0300

    review fixing

--- src/components/surface/Surface.stories.tsx
+++ src/components/surface/Surface.stories.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
 import { Meta } from "@storybook/react";
-import Surface from "./Surface";
+import Surface, { Align } from "./Surface";
 
 export default {
   title: "Components/Surface",
   component: Surface,
 } as Meta;
 
-export const Primary: React.VFC<Record<string, never>> = () => (
+export const Center: React.VFC<Record<string, never>> = () => (
   <Surface>Children here</Surface>
+);
+
+export const Right: React.VFC<Record<string, never>> = () => (
+  <Surface align={Align.RIGHT}>Children here</Surface>
+);
+
+export const Left: React.VFC<Record<string, never>> = () => (
+  <Surface align={Align.LEFT}>Children here</Surface>
 );
--- src/components/surface/Surface.tsx
+++ src/components/surface/Surface.tsx
@@ -1,19 +1,34 @@
 import * as React from "react";
 
-type Props = {
-  children: React.ReactNode;
-  backgroundColor?: string;
-  layoutClass?: string;
+export const Align = {
+  LEFT: "justify-start",
+  RIGHT: "justify-end",
+  CENTER: "justify-center",
+} as const;
+
+type StaticProperties = {
+  Align: typeof Align;
 };
 
-const Surface: React.FC<Props> = ({
+type Values<T> = T[keyof T];
+
+type Props = {
+  children: React.ReactNode;
+  layoutClass?: string;
+  align?: Values<typeof Align>;
+};
+
+const Surface: React.FC<Props> & StaticProperties = ({
   children,
-  backgroundColor = "bg-white",
-  layoutClass = "max-w-xl w-full m-auto",
+  align = Align.CENTER,
 }) => (
-  <div className={`w-full py-10 ${backgroundColor}`}>
-    <div className={layoutClass}>{children}</div>
+  <div className="w-full py-10 bg-white">
+    <div className={`flex ${align}`}>
+      <div className="max-w-xl w-full">{children}</div>
+    </div>
   </div>
 );
+
+Surface.Align = Align;
 
 export default Surface;
