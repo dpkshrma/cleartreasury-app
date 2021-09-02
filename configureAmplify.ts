6506fe3f67330a99d94f48b71911b4f612386724
Rory Powis <rpowis@users.noreply.github.com>
2021-09-02 11:06:05 +0200

    Update AppSync API config

--- configureAmplify.ts
+++ configureAmplify.ts
@@ -19,10 +19,10 @@ export const config = {
   },
   API: {
     aws_appsync_graphqlEndpoint:
-      "https://ojljqi4kdjb3ve2qk5ghmw3veu.appsync-api.eu-west-2.amazonaws.com/graphql",
+      "https://5mjjrvstp5b23fu3jv5toia4f4.appsync-api.eu-west-2.amazonaws.com/graphql",
     aws_appsync_region: "eu-west-2",
     aws_appsync_authenticationType: "API_KEY",
-    aws_appsync_apiKey: "da2-zoa4vdfdozdpjbqgj3vgwu44mm",
+    aws_appsync_apiKey: "da2-n2ilir73sjcwfkutvgbxy6tsae",
   },
 };
 
