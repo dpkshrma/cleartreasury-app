5509b4a34d31321a8aac2e4bc5b19091541f141d
Emre Gozel <emre@gozel.org>
2021-09-20 15:56:52 +0300

    beneficiaries graphql added

--- src/mocks/handlers/beneficiaryHandlers.ts
+++ src/mocks/handlers/beneficiaryHandlers.ts
@@ -1,4 +1,5 @@
 import { graphql } from "msw";
+import beneficiaries from "../data/beneficiaries";
 
 export const beneficiaryHandlers = [
   graphql.mutation("createBeneficiary", (req, res, ctx) => {
@@ -8,6 +9,35 @@ export const beneficiaryHandlers = [
           id: "1",
           message: "Beneficiary added to client's list",
         },
+      })
+    );
+  }),
+
+  graphql.query("getBeneficiaries", (req, res, ctx) => {
+    let returnedBeneficiaries = [];
+
+    // In test we can filter for email
+    const testBeneficiaries = beneficiaries.filter(
+      (beneficiary) => beneficiary.client_ref === req.body.variables.client_ref
+    );
+
+    if (testBeneficiaries.length) {
+      returnedBeneficiaries = testBeneficiaries;
+    } else {
+      // Return 2 clients
+      const fakeBeneficiaries = beneficiaries
+        .filter((beneficiary) => beneficiary.client_ref === "123456")
+        .map((beneficiary) => ({
+          ...beneficiary,
+          client_ref: req.body.variables.client_ref,
+        }));
+
+      returnedBeneficiaries = fakeBeneficiaries;
+    }
+
+    return res(
+      ctx.data({
+        getBeneficiaries: returnedBeneficiaries,
       })
     );
   }),
