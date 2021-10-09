fcb3897dc7cce6c9a50e411c1754965eed938953
Rory Powis <rpowis@users.noreply.github.com>
2021-10-09 16:42:30 +0200

    Decouple API tests from our actual API queries and mutations

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "!**/*.d.ts",
     "!**/node_modules/**",
   ],
+  setupFiles: ["dotenv/config"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/", "/infra/"],
   modulePathIgnorePatterns: ["/infra/"],
